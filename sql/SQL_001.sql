236fd9f27142b2fc9e301c7f406525b6e7475a83
Roberto Dias <robertordias162@gmail.com>
2020-05-05 16:10:15 -0300

    inclusao de arquivos sql

---
+++ sql/SQL_001.sql
@@ -0,0 +1,32 @@
+create database timetomovie_dev;
+use timetomovie_dev;
+
+create table local(
+	id_local int not null auto_increment primary key,
+    uf varchar(2),
+    estado varchar(50)
+);
+
+create table usuario(
+	id_usuario int not null auto_increment primary key,
+    nome varchar(50),
+    email varchar(100),
+    senha varchar(1000),
+    id_local int,
+    foreign key (id_local) references local(id_local)
+);
+
+create table semana(
+	id_semana int not null auto_increment primary key,
+    dia varchar(20),
+    id_usuario int,
+    foreign key (id_usuario) references usuario(id_usuario)
+);
+
+create table horario(
+	id_horario int not null auto_increment primary key,
+    horario varchar(20),
+    id_semana int,
+    foreign key (id_semana) references semana(id_semana)
+);
+
